17eb807dec73bd040d0211b488ddf807c3c02431
Misael <misael@qq.com>
2021-12-25 15:22:49 +0800

    Delete YetiForce CRM 5.x.x directory

--- YetiForce CRM 5.x.x/5.1.0_ServicePack/updates/deleteFiles.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-	'modules/API/cron/CalDav.php',
-	'modules/API/cron/CardDav.php',
-];
--- YetiForce CRM 5.x.x/5.2.0_to_5.3.0/updates/files/config/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-	'appVersion' => '5.3.0',
-	'patchVersion' => '2020.03.13',
-	'lib_roundcube' => '0.0.87'
-];
